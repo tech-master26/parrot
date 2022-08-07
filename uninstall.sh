6e4d2acc65554e522042fd2a3e463312ae891900
MD. Sirajul Islam <106694080+sirajul26@users.noreply.github.com>
2022-08-07 20:21:17 +0600

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
-#This Script id created by TermuxProfessor
+#This Script id Modified by MD.Sirajul Islam
 #Don't copy or modify code Read License First
-#Subscribe me on YouTube www.youtube.com/c/TermuxProfessor
+#Subscribe me on YouTube www.youtube.com/c/AndroidTips-&-trick's
 clear
 echo ""
 echo -e "         \e[1;91m__\e[1;92m,-\e[1;93m////\e[1;92m, "
@@ -16,8 +16,8 @@ echo -e "                 \ \'      "
 echo -e "                  \ \     "
 echo -e "                   '-'    \e[1;97m "
 echo ""
-echo -e "\e[1;91m [+] YouTube: \e[1;92mTermuxProfessor"
-echo -e "\e[1;91m [+] Github: \e[1;92mtermuxprofessor\e[1;97m"
+echo -e "\e[1;91m [+] YouTube: \e[1;92mAndroidTips-&-Trick's"
+echo -e "\e[1;91m [+] Github: \e[1;92sirajul26\e[1;97m"
 echo ""
 read -p " Does you want to Uninstall Parrot Shell?(Yes/No) : " input
 
